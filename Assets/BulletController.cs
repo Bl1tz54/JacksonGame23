a43b9172aed8c571b82c20398a27d04dcb173450
Jackson Meyer <jacksonmeyer@mmc.school.nz>
2023-08-09 13:57:03 +1200

    updated bullet controller script
    the bullets now dissappear after 4 seconds of being spawned in

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -4,64 +4,26 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    public float bulletSpeed = 5f;
-    public int maxRicochetCount = 2; // Maximum number of times the bullet can ricochet
-    public float interpolationFactor = 0.5f; // Controls the smoothness of interpolation
-
-    private Rigidbody2D rb;
-    private int ricochetCount = 0;
-    private bool isColliding = false;
-
-    private void Awake()
+    public class BulletLifetime : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
+        public float lifetime = 4f; // The time in seconds before the bullet is destroyed
 
-    private void OnEnable()
-    {
-        rb.velocity = transform.up * bulletSpeed;
-        ricochetCount = 0;
-        isColliding = false;
-    }
-
-    private void FixedUpdate()
-    {
-        if (isColliding)
+        private void Start()
         {
-            // Interpolate the bullet position based on the interpolationFactor
-            Vector2 interpolatedPosition = rb.position + rb.velocity * Time.fixedDeltaTime * interpolationFactor;
-            rb.MovePosition(interpolatedPosition);
+            // Start a coroutine to destroy the bullet after the specified lifetime
+            StartCoroutine(DestroyAfterLifetime());
+        }
+
+        private IEnumerator DestroyAfterLifetime()
+        {
+            // Wait for the specified lifetime duration
+            yield return new WaitForSeconds(lifetime);
+
+            // Destroy the bullet game object
+            Destroy(gameObject);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (ricochetCount < maxRicochetCount)
-        {
-            // Calculate the reflection direction
-            Vector2 incomingVelocity = rb.velocity.normalized;
-            Vector2 normal = collision.contacts[0].normal;
-            Vector2 reflection = CalculateReflection(incomingVelocity, normal).normalized;
-            rb.velocity = reflection * bulletSpeed;
-            ricochetCount++;
-            isColliding = true;
-        }
-        else
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        isColliding = false;
-    }
-
-    private Vector2 CalculateReflection(Vector2 incoming, Vector2 normal)
-    {
-        // Reflect the incoming velocity smoothly
-        return Vector2.Reflect(incoming, normal) * (1f - interpolationFactor) - rb.velocity * interpolationFactor;
-    }
 }
 
 
