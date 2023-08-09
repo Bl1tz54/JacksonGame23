d3dd803a7e90ca029d1a73d3f57e849a1d53d610
Jackson Meyer <jacksonmeyer@mmc.school.nz>
2023-08-10 10:33:24 +1200

    Bullets get destroyed after 4 seconds of being spawned in
    added a bulletlifetime script to my bullet object

---
+++ Assets/BulletLifetime.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BulletLifetime : MonoBehaviour
+{
+    public float lifetime = 4f; // The time in seconds before the bullet is destroyed
+
+    private void Start()
+    {
+        // Start a coroutine to destroy the bullet after the specified lifetime
+        StartCoroutine(DestroyAfterLifetime());
+    }
+
+    private IEnumerator DestroyAfterLifetime()
+    {
+        // Wait for the specified lifetime duration
+        yield return new WaitForSeconds(lifetime);
+
+        // Destroy the bullet game object
+        Destroy(gameObject);
+    }
+}
